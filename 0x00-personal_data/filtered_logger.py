4bcf8ed8201d0ec85d773f58fdf4ef68fb881cd8
cyrusDev1 <cyrushoungue1240@gmail.com>
2023-02-13 16:13:52 +0100

    upate tasks next

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,8 @@
 log message obfuscated"""
 from typing import List
 import re
+import logging
+PII_FIELDS = ('name', 'password', 'phone', 'ssn', 'email')
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -13,3 +15,36 @@ def filter_datum(fields: List[str], redaction: str,
             f'{field}=.*?{separator}',
             f'{field}={redaction}{separator}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ filters values in incoming log records using filter_datum"""
+        return filter_datum(self.fields, self.REDACTION,
+                            super(RedactingFormatter, self).format(record),
+                            self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    """ returns a logging object"""
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    stream_handler = logging.StreamHandler()
+    logger.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(stream_handler)
+
+    return logger
+#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
